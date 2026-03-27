2a3017557d7e1c4d02a800ba6ef6f620c11151a5
Firefly <nerdtoys69@gmail.com>
2026-03-27 16:02:31 -0400

    Fix CMake git commands to use WORKING_DIRECTORY

--- build_test/lib/SDL2/include/SDL2/SDL_revision.h
+++ build_test/lib/SDL2/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-v5.12.0-1-g57f299e9 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-v5.12.0-3-ge92f942 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-v5.12.0-1-g57f299e9"
+#define SDL_REVISION "SDL-v5.12.0-3-ge92f942"
 #endif
